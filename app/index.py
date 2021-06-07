26e9d7d21b55c0734667e038a8ed14adbebf73d8
ismael-su <ismael@rumble.studio>
2021-06-07 10:52:17 +0100

    refactor(statics) removed statics folder

--- app/index.py
+++ app/index.py
@@ -1,20 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows,
-# actions, and settings.
 
 from fastapi import FastAPI
-from starlette.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
 
 @app.get("/")
 def test():
-    return 'This is just a test, making sure we can run our container'
+    return 'Hello World'
 
 
 @app.get("/users/dev")
@@ -28,15 +20,3 @@ def get_devs():
         'data': dev_list
     }
     return response
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('RS Server starting')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
